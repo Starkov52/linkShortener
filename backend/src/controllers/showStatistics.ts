920ad3505ceef4509d6a0685e929499f1478ec75
Starkov52 <valuznnicartem@gmail.com>
2025-09-05 18:11:42 +0300

    code rafactoring

--- backend/src/controllers/showStatistics.ts
+++ backend/src/controllers/showStatistics.ts
@@ -3,12 +3,12 @@ import { getLinkStatistic } from "../services/link/getLinkStatistics";
 import { ShortLinkType } from "../types";
 export async function showStatistics(request: Request, response: Response) {
     const linkId: string | undefined = request.params.linkId;
-    if (!linkId) return response.json("Отсутсвует параметр 'linkId'");
+    if (!linkId) return response.status(400).json("Отсутсвует параметр 'linkId'");
     const shortLink: string = "http://localhost:3000/shortLink/" + linkId;
     try {
         const linkStatistics: ShortLinkType | null = await getLinkStatistic(shortLink);
         if (!linkStatistics) {
-            return response.json({ message: "Ссылка не обнаружена" });
+            return response.status(400).json({ message: "Ссылка не обнаружена" });
         }
         console.log(linkStatistics);
         response.json(linkStatistics);
